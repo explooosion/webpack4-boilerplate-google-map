79d5009edf71260f0fed5e6699a3bd5a99afc103
Robby <ta7382@gmail.com>
2018-10-20 17:36:43 +0800

    Add comments

--- src/js/map.js
+++ src/js/map.js
@@ -6,41 +6,57 @@ class Map {
         this.markers = [];
     }
 
+    /**
+     * 初始化地圖
+     * @param {number} lat 
+     * @param {number} lng 
+     * @param {number} zoom 
+     */
     init(lat, lng, zoom) {
         this.map = new google.maps.Map(document.querySelector('.map'), {
             center: { lat: lat, lng: lng },
             zoom: zoom,
         });
         this.map.addListener('click', this.onClick);
-
         this.geocoder = new google.maps.Geocoder;
     }
 
+    /**
+     * 地圖 Click 事件
+     * @param {*} args 
+     */
     onClick(args) {
         const { latLng } = args;
         console.log('click', latLng.lat(), latLng.lng());
         return latLng;
     }
 
+    /**
+     * 新增標記點
+     * @param {object} location 
+     * @param {number} zoom 
+     */
     addMarker(location, zoom) {
-
         // 移除當前標記
         if (this.centerMarker) {
             this.centerMarker.setMap(null);
         }
-
         this.centerMarker = new google.maps.Marker({
             position: location,
             map: this.map,
         });
-
         // 將定位點置中
         this.map.setCenter(location);
         this.map.setZoom(zoom || 14);
     }
 
+    /**
+     * 地址搜尋經緯度
+     * @param {string} address 
+     * @param {object} callback 
+     */
     search(address, callback) {
-        return this.geocoder.geocode({ 'address': address }, (results, status) => {
+        this.geocoder.geocode({ 'address': address }, (results, status) => {
             if (status == google.maps.GeocoderStatus.OK) {
                 const location = results[0].geometry.location;
                 const lat = Number(results[0].geometry.location.lat()).toFixed(6);
