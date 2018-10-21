81bf8695bbe873daf0f101c597f5c5af17c320f9
Robby <ta7382@gmail.com>
2018-10-21 13:56:46 +0800

    Add color for circle with color picker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import scss
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/index.scss';
 
 // import libs
 import $ from 'jquery';
 import 'bootstrap';
+import '@claviska/jquery-minicolors';
 
 // import js
 import Map from './lib/map';
@@ -18,13 +18,14 @@ console.log($('h1').text());
 console.log(document.querySelector('h1').innerText);
 
 // initial map
-const lat = 24.799448;
-const lng = 120.979021;
-const zoom = 14;
+const lat = 25.048853;
+const lng = 121.518709;
+const zoom = 17;
 const map = new Map();
 
 window.onload = () => {
     map.init(lat, lng, zoom);
+    searchLocation();
 };
 
 // Search Event
@@ -33,10 +34,20 @@ $('#txtAdd').keypress(e => {
     if (e.keyCode === 13) searchLocation();
 });
 
-// Update circle
-$('#txtRange').bind('keyup mouseup', (e) => {
+// circle update
+$('#txtRange').bind('keyup mouseup', e => {
     if (!map.centerCircle) return;
     map.centerCircle.setRadius(Number(e.target.value));
+});
+
+// circle color and update
+$('#txtColor').minicolors();
+$('#txtColor').change(e => {
+    if (!map.centerCircle) return;
+    map.centerCircle.setOptions({
+        strokeColor: e.target.value,
+        fillColor: e.target.value,
+    });
 });
 
 function searchLocation() {
@@ -49,7 +60,11 @@ function searchLocation() {
 
         // 定位地址
         map.addMarker(location, 18);
-        map.addCircle(location, $('#txtRange').val());
+        map.addCircle(
+            location,
+            $('#txtRange').val(),
+            $('#txtColor').val()
+        );
 
         // 設定經緯度標籤
         $('#txtLat').text(`Lat：${lat}`);
--- src/lib/map.js
+++ src/lib/map.js
@@ -56,16 +56,16 @@ class Map {
      * @param {object} location 
      * @param {number} radius 
      */
-    addCircle(location, radius) {
+    addCircle(location, radius, color = '#FF0000') {
         // 移除當前標記
         if (this.centerCircle) {
             this.centerCircle.setMap(null);
         }
         this.centerCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
+            strokeColor: color,
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: '#FF0000',
+            fillColor: color,
             fillOpacity: 0.35,
             map: this.map,
             center: location,
